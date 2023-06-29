dd37537ab90874c5e0cc86ea47a7ea9a6c3d769a
Natan <natancom2n@gmail.com>
2023-06-29 03:08:25 +0100

    modify about error

---
+++ lib/hello.ex
@@ -0,0 +1,7 @@
+defmodule Exercism.Hello do
+
+  def print_tela(var) do
+     {:ok, result} = {:ok, var}
+
+  end
+end
--- test/exercism_test.exs
+++ test/exercism_test.exs
@@ -3,14 +3,23 @@ defmodule ExercismTest do
   # doctest Exercism
 
   describe "hello" do
-    test "when a valid variable is provide, returns the same variable" do
+    test "when function is call, return the text Hello World " do
 
       expected_response  = "Hello, World"
 
       assert Exercism.hello == expected_response
     end
-
   end
 
+  describe "print_tela/1" do
+
+    test "when a valid variable is provide, returns the same variable" do
+
+      expected_response = {:ok, "hello"}
+      assert Exercism.Hello.print_tela("hello") == expected_response
+    end
+
+
+  end
 
 end
